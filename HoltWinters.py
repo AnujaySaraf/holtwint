895f55c5afb0eb040d24dc4f12dfaccb566ad540
Eric Lundquist <e.t.lundquist@gmail.com>
2016-03-17 12:12:00 -0500

    added optional optimization

--- HoltWinters.py
+++ HoltWinters.py
@@ -7,8 +7,8 @@ import pandas as pd
 from sklearn        import linear_model
 from scipy.optimize import fmin_l_bfgs_b
 
-# bring in the passenger data from HW4 and the hourly car counts from PJ2
-#------------------------------------------------------------------------
+# bring in the passenger data from HW4 to test the function against R output
+#---------------------------------------------------------------------------
 
 sdata = open('sampledata.csv')
 tsA = sdata.read().split('\n')
@@ -16,39 +16,48 @@ tsA = list(map(int, tsA))
 
 # define main function [holtWinters] to generate retrospective smoothing/predictions
 #-----------------------------------------------------------------------------------
-
-def holtWinters(ts, p, sp, ahead):
-    '''additive HoltWinters retrospective smoothing & prediction algorithm
-    @ts[list]:     time series of data to model
-    @p[int]:       period of the time series (for calculation of seasonal effects)
-    @sp[int]:      number of starting periods to use when calculating initial parameter values
-    @ahead[int]:   number of future periods for which predictions will be generated
+ 
+def holtWinters(ts, p, sp, ahead, alpha = None, beta = None, gamma = None):
+    '''HoltWinters retrospective smoothing & future period prediction algorithm 
+       currently only the additive model is implemented and the (alpha, beta, gamma)
+       parameters have to be either all user chosen or all optimized via one-step-ahead MSD 
+       initial (a, b, s) values calculated with a fixed-period seasonal decomposition and a
+       simple linear regression to recover the initial level (B0) and initial slope (B1)
+    @params:
+        - ts[list]:     time series of data to model
+        - p[int]:       period of the time series (for the calculation of seasonal effects)
+        - sp[int]:      number of starting periods to use when calculating initial parameter values
+        - ahead[int]:   number of future periods for which predictions will be generated
+        - alpha[float]: user-specified level  forgetting factor (optimized if None)
+        - beta[float]:  user-specified slope  forgetting factor (optimized if None)
+        - gamma[float]: user-specified season forgetting factor (optimized if None)
     @return: 
-        - @alpha[float]:    optimal level forgetting factor estimated via optimization
-        - @beta[float]:     optimal trend forgetting factor estimated via optimization
-        - @gamma[float]:    optimal season forgetting factor estimated via optimization
-        - @MSD[float]:      optimal Mean Square Deviation with respect to one-step-ahead predictions
-        - @params[tuple]:   final (a,b,s) parameter values used for prediction of future observations
+        - @alpha[float]:    chosen/optimal level  forgetting factor used in calculations
+        - @beta[float]:     chosen/optimal trend  forgetting factor used in calculations
+        - @gamma[float]:    chosen/optimal season forgetting factor used in calculations
+        - @MSD[float]:      chosen/optimal Mean Square Deviation with respect to one-step-ahead predictions
+        - @params[tuple]:   final (a,b,s) parameter values used for the prediction of future observations
         - @smoothed[list]:  smoothed values (Level + Trend + Seasonal) for the original time series
-        - @predicted[list]: predicted values for the next @ahead periods in the time series
-    sample call:
-        results = holtWinters(ts, 12, 4, 24)'''
+        - @predicted[list]: predicted values for the next @ahead periods of the time series
+    sample calls:
+        results = holtWinters(ts, 12, 4, 24)
+        results = holtWinters(ts, 12, 4, 24, alpha = 0.1, beta = 0.2, gamma = 0.3)'''
 
-    ituning = [0.1, 0.1, 0.1]
-    ibounds = [(0,1), (0,1), (0,1)]
     a, b, s = _initValues(ts, p, sp)
-    
-    optimized = fmin_l_bfgs_b(_MSD, ituning, args = (ts, p, a, b, s[:]), bounds = ibounds, approx_grad = True, iprint = 0)
-    alpha, beta, gamma = optimized[0]
-    MSD = optimized[1]
 
-    smoothed, params = _expSmooth(ts, p, a, b, s[:], alpha, beta, gamma)
+    if alpha == None or beta == None or gamma == None:
+        ituning   = [0.1, 0.1, 0.1]
+        ibounds   = [(0,1), (0,1), (0,1)]
+        optimized = fmin_l_bfgs_b(_MSD, ituning, args = (ts, p, a, b, s[:]), bounds = ibounds, approx_grad = True)
+        alpha, beta, gamma = optimized[0]
+
+    MSD, params, smoothed = _expSmooth(ts, p, a, b, s[:], alpha, beta, gamma)
     predicted = _predictValues(p, ahead, params)
 
     return {'alpha': alpha, 'beta': beta, 'gamma': gamma, 'MSD': MSD, 'params': params, 'smoothed': smoothed, 'predicted': predicted}
 
 def _initValues(ts, p, sp):
-    '''subroutine to calculate the initial parameter values (a, b, s) based on a fixed number of starting periods'''
+    '''subroutine to calculate initial parameter values (a, b, s) based on a fixed number of starting periods'''
 
     initSeries = pd.Series(ts[:p*sp])
     rawSeason  = initSeries - pd.rolling_mean(initSeries, window = p, min_periods = p, center = True)
@@ -83,7 +92,7 @@ def _MSD(params, *args):
 def _expSmooth(ts, p, a, b, s, alpha, beta, gamma):
     '''subroutine to calculate the retrospective smoothed values and final parameter values for prediction'''
 
-    smoothed      = []
+    smoothed = []
     Lt1, Tt1, St1 = a, b, s[:]
 
     for t in range(len(ts)):
@@ -95,7 +104,8 @@ def _expSmooth(ts, p, a, b, s, alpha, beta, gamma):
         smoothed.append(Lt1 + Tt1 + St1[t % p])
         Lt1, Tt1, St1[t % p] = Lt, Tt, St
 
-    return smoothed, (Lt1, Tt1, St1)
+    MSD = sum([(ts[t] - smoothed[t])**2 for t in range(len(smoothed))])/len(smoothed)
+    return MSD, (Lt1, Tt1, St1), smoothed
 
 def _predictValues(p, ahead, params):
     '''subroutine to generate predicted values @ahead periods into the future'''
@@ -106,6 +116,7 @@ def _predictValues(p, ahead, params):
 # print out the results to check against R output
 #------------------------------------------------
 
+results = holtWinters(tsA, 12, 4, 24, alpha = 0.1, beta = 0.1, gamma = 0.1)
 results = holtWinters(tsA, 12, 4, 24)
 
 print("TUNING: ", results['alpha'], results['beta'], results['gamma'], results['MSD'])
